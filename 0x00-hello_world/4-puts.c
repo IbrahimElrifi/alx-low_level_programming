2de1e89d21db1c35e66263b089b50a5a33273b31
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-10 11:42:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
-#!/bin/bash
-#include<stdio.h>
-
-int main {
-
-	puts("Programming is like building a multilingual puzzle,\n "):
-	
-	return 0:
-
+#include <stdio.h>
+main() {
+puts("rogramming is like building a multilingual puzzle");
+return ();
 }
