fec4b9b96ac24beb21959ba0e89cd0627c016c2e
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-15 16:39:01 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	printchar("_putchar");
-	return (0);
+    print_alphabet();
+    return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,17 @@
+#include <stdio.h>
+
 void printchar(char n[])
 {
 	printf("%s\n", n);
+}
+
+void print_alphabet(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a' ; lowercase <= 'z' ; lowercase++)
+	{
+		_putchar(lowercase);
+	}
+	_putchar('\n');
 }
