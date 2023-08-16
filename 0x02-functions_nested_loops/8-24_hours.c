1f3159b672a8d553b0346c8613872f302463be9f
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-16 14:20:41 -0700

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of an number
+ * @n: integer to check
+ * Return: n % 10 to get the last digit
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	h = 0;
+	m = 0;
+
+for (h = 0; h <= 24; h++)
+{
+	for (m = 0; m < 60; m++)
+	{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+	}
+}
+return (0);
+}
