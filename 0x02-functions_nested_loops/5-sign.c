b539d9675c25b339e9e221daeffdf6a61bc625a7
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-16 11:30:17 -0700

    check

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - if number is greater or lesser than zero
+ * @n: character to check
+ * Return: 1 and print '+' if greater than 0 eles if is less -1and print '-' ,
+ * 0 and print '0' if not
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+	_putchar('+');
+	_putchar(',');
+	_putchar(' ');
+	return (1);
+}
+else if (n < 0)
+{
+	_putchar('-');
+	_putchar(',');
+	_putchar(' ');
+	return (-1);
+}
+else
+{
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	return (0);
+}
+_putchar('\n');
+}
