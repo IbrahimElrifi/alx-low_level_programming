de9c0851dd7b7a72f476eb590ea382b61801b257
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-18 14:27:05 -0700

    $commitMessage

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if  is a digit or not
+ *
+ * Return: 1 if c is a digit 0 if not.
+ */
+
+int _isdigit(int c)
+{
+
+if (c >= 0 && c <= 9)
+{
+	return (1)
+} 
+else
+{
+	return (0)
+}
+
+
+
+
+}	
