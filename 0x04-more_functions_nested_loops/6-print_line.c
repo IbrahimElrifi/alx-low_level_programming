80fef3b597a7971a8669e1af7610d0faedd36b65
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-18 19:07:10 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal lines
+ * @n: integer number
+ * Return: a number of diagonal lines according to n
+ */
+
+void print_diagonal(int n)
+{
+	char l = '_';
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+		_putchar('\n');
+		}
+	_putchar(l);
+	}
+}
