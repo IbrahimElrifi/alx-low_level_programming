4761717d5082153ae199eb8fcacc63fe683ab002
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-22 19:30:32 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _puts - print string.
+ *
+ *@str: a pointer
+ *
+ *Return: .
+ */
+
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+
+}
