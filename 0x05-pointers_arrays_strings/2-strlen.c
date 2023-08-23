7ab06378c0368d3b9770d0a95ba578ee17ae7255
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-22 18:40:45 -0700

    len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - reset the value of int variable a and b.
+ *
+ *@a: a pointer
+ *
+ *@b: a pointer
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i);
+}
