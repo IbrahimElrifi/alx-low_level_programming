3ff9b326f00878965ac2c6bc431f31ff65e34c1b
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-08-22 18:12:22 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int - reset the value of int variable a and b.
+ *
+ *@a: a pointer
+ *
+ *@b: a pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
